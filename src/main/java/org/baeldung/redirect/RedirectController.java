37bd24b2f669ca1d6e2829d3bd70c0ea8c3e42e7
alex-semenyuk <alexemenyuk88@gmail.com>
2015-07-13 21:52:10 +0200

    Initial commit

---
+++ src/main/java/org/baeldung/redirect/RedirectController.java
@@ -0,0 +1,38 @@
+package org.baeldung.redirect;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
+
+@Controller
+@RequestMapping("/")
+public class RedirectController {
+
+	@RequestMapping(value="/testRedirectWithRedirectPrefix",method = RequestMethod.GET)
+	public ModelAndView testRedirectWithRedirectPrefix(ModelMap model) {
+		model.addAttribute("attribute", "testRedirectWithRedirectPrefix");
+		return new ModelAndView("redirect:/redirectedUrl", model);
+	}
+
+	@RequestMapping(value="/testRedirectWithRedirectView", method = RequestMethod.GET)
+	public RedirectView testRedirectWithRedirectView(ModelMap model) {
+		model.addAttribute("attribute", "testRedirectWithRedirectView");
+		return new RedirectView("redirectedUrl");
+	}
+
+	@RequestMapping(value="/testRedirectWithForwardPrefix",method = RequestMethod.GET)
+	public ModelAndView testRedirectWithForwardPrefix(ModelMap model) {
+		model.addAttribute("attribute", "testRedirectWithForwardPrefix");
+		return new ModelAndView("forward:/redirectedUrl", model);
+	}
+
+	@RequestMapping(value="/redirectedUrl", method = RequestMethod.GET)
+	public ModelAndView redirection(ModelMap model) {
+		model.addAttribute("message", "Test redirection");
+		return new ModelAndView("redirect", model);
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/pages/redirect.jsp
@@ -0,0 +1,6 @@
+<html>
+<body>
+    <h1>${attribute}</h1>
+	<h1>${message}</h1>
+</body>
+</html>
